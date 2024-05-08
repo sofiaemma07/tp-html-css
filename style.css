c4776365f1b3ab30a8dcfd9dbd4e0de9a40df972
sofiaemma07 <sofiaemmagalindez@gmail.com>
2024-05-08 17:44:58 -0300

    agrega style.css

---
+++ style.css
@@ -0,0 +1,43 @@
+* {
+  font-family: Monaco,'Courier New',monospace;
+}
+
+.images {
+  margin: 0 auto;
+  width: 50%;
+  gap: 2em;
+  display: grid;
+  grid-template: 1fr 1fr / 1fr 1fr 1fr;
+}
+
+.container {
+  display: flex;
+  gap: 1em;
+}
+html, body {
+  margin: 0;
+  min-height: 100vh;
+}
+
+aside {
+  width: 40%;
+}
+
+ul {
+  list-style: none;
+}
+
+footer {
+  background: black;
+  color: white;
+  text-align: center;
+}
+
+.brand {
+  display: flex;
+  gap: 5px;
+}
+
+.brand img {
+  border-radius: 50%;
+}
